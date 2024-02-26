b561efb736df62fa8181e3b622046c14e9bb54b6
Maya Raskin <mayaraskin14@gmail.com>
2024-02-26 22:26:50 +0200

    Implemented RegisterHttpRequestHandler

--- http-server/HttpServer.h
+++ http-server/HttpServer.h
@@ -17,10 +17,12 @@ namespace mta_http_server {
         std::string host_;
         std::uint16_t port_;
         bool running_;
+        std::map<Uri, std::map<HttpMethod, HttpRequestHandler_t>> request_handlers_;
 
     public:
         HttpServer() = default;
-        explicit HttpServer(const std::string& host, std::uint16_t port) : host_(host), port_(port) { }
+        explicit HttpServer(const std::string& host, std::uint16_t port) : 
+            host_(host), port_(port), running_(false), request_handlers_() { }
         HttpServer(HttpServer&&) = default;
         HttpServer& operator=(HttpServer&&) = default;
         ~HttpServer() = default;
@@ -28,6 +30,15 @@ namespace mta_http_server {
         void Start();
         void Listen();
         void Stop();
+
+        void RegisterHttpRequestHandler(const std::string& path, HttpMethod method,
+            const HttpRequestHandler_t& callback) {
+            request_handlers_[Uri(path)].insert(std::make_pair(method, callback));
+        }
+        void RegisterHttpRequestHandler(const Uri& uri, HttpMethod method,
+            const HttpRequestHandler_t& callback) {
+            request_handlers_[uri].insert(std::make_pair(method, callback));
+        }
 
         std::string host() const { return host_; }
         std::uint16_t port() const { return port_; }
