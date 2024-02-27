10598debb28d8de7dab57ceca8b3a12cacbffb0f
Maya Raskin <mayaraskin14@gmail.com>
2024-02-27 12:50:47 +0200

    Implemented ProcessEvents

--- http-server/HttpServer.cpp
+++ http-server/HttpServer.cpp
@@ -1,81 +1,6 @@
 #include "HttpServer.h"
 
 namespace mta_http_server {
-	void HttpServer::Start() {
-		WSAData wsa_data;
-
-		if (!port)
-			throw std::runtime_error("Port is not configured!");
-
-		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsa_data))
-			throw std::runtime_error("Error at WSAStartup");
-
-		SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-		if (INVALID_SOCKET == listen_socket) {
-			int last_err = WSAGetLastError();
-			WSACleanup();
-			throw std::runtime_error("Error at socket(): " + last_err);
-		}
-
-		server_service_.sin_family = AF_INET;
-		server_service_.sin_addr.s_addr = INADDR_ANY;
-		server_service_.sin_port = htons(port_);
-
-		if (SOCKET_ERROR == bind(listen_socket, (SOCKADDR*)&server_service_, sizeof(server_service_)))
-		{
-			int last_err = WSAGetLastError();
-			closesocket(listen_socket);
-			WSACleanup();
-			throw std::runtime_error("Error at bind(): " + last_err);
-		}
-
-		Listen(listen_socket);
-	}
-
-	void HttpServer::Listen(SOCKET& listen_socket) {
-		if (SOCKET_ERROR == listen(listen_socket, 5)) {
-			int last_err = WSAGetLastError();
-			closesocket(listen_socket);
-			WSACleanup();
-			throw std::runtime_error("Time Server: Error at listen(): " + last_err);
-		}
-
-		socket_service_.addSocket(listen_socket, LISTEN);
-		running_ = true;
-	}
-
-	HttpResponse HttpServer::HandleHttpRequest(const HttpRequest& request) {
-		auto it = request_handlers_.find(request.uri());
-		if (it == request_handlers_.end())  // this uri is not registered
-			return HttpResponse(HttpStatusCode::NotFound);
-
-		auto callback_it = it->second.find(request.method());
-
-		if (callback_it == it->second.end()) // no handler for this method
-			return HttpResponse(HttpStatusCode::MethodNotAllowed);
-
-		return callback_it->second(request);  // call handler to process the request
-	}
-
-	SOCKET_STATE* SocketService::findListeningSocket() {
-		for (auto& item : sockets_) {
-			if (item.recv == SocketFunction::LISTEN)
-				return &item;
-		}
-
-		return nullptr;
-	}
-
-	void HttpServer::ProcessEvents() {
-		while (running_) {
-			//TODO: Not implemented
-		}
-
-		closesocket(socket_service_.findListeningSocket()->id);
-		WSACleanup();
-	}
-
 	bool SocketService::addSocket(SOCKET id, SocketFunction what) {
 		for (int i = 0; i < Settings::MAX_SOCKETS; i++) {
 			if (sockets_[i].recv == SocketFunction::EMPTY) {
@@ -205,7 +130,7 @@ namespace mta_http_server {
 		//}
 
 		bytesSent = send(msgSocket, sendBuff, (int)strlen(sendBuff), 0);
-		
+
 		if (SOCKET_ERROR == bytesSent) {
 			std::cout << "Time Server: Error at send(): " << WSAGetLastError() << std::endl;
 			return;
@@ -214,5 +139,124 @@ namespace mta_http_server {
 		std::cout << "Time Server: Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
 
 		sockets_[index].send = IDLE;
+	}
+
+	void HttpServer::Start() {
+		WSAData wsa_data;
+
+		if (!port)
+			throw std::runtime_error("Port is not configured!");
+
+		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsa_data))
+			throw std::runtime_error("Error at WSAStartup");
+
+		SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+		if (INVALID_SOCKET == listen_socket) {
+			int last_err = WSAGetLastError();
+			WSACleanup();
+			throw std::runtime_error("Error at socket(): " + last_err);
+		}
+
+		socket_service_.SetServerService(AF_INET, INADDR_ANY, port());
+
+		if (SOCKET_ERROR == bind(listen_socket, (SOCKADDR*)&socket_service_.server_service(), sizeof(socket_service_.server_service()))) {
+			int last_err = WSAGetLastError();
+			closesocket(listen_socket);
+			WSACleanup();
+			throw std::runtime_error("Error at bind(): " + last_err);
+		}
+
+		Listen(listen_socket);
+	}
+
+	void HttpServer::Listen(SOCKET& listen_socket) {
+		if (SOCKET_ERROR == listen(listen_socket, 5)) {
+			int last_err = WSAGetLastError();
+			closesocket(listen_socket);
+			WSACleanup();
+			throw std::runtime_error("Time Server: Error at listen(): " + last_err);
+		}
+
+		socket_service_.addSocket(listen_socket, LISTEN);
+		running_ = true;
+	}
+
+	HttpResponse HttpServer::HandleHttpRequest(const HttpRequest& request) {
+		auto it = request_handlers_.find(request.uri());
+		if (it == request_handlers_.end())  // this uri is not registered
+			return HttpResponse(HttpStatusCode::NotFound);
+
+		auto callback_it = it->second.find(request.method());
+
+		if (callback_it == it->second.end()) // no handler for this method
+			return HttpResponse(HttpStatusCode::MethodNotAllowed);
+
+		return callback_it->second(request);  // call handler to process the request
+	}
+
+	SOCKET_STATE* SocketService::findListeningSocket() {
+		for (auto& item : sockets_) {
+			if (item.recv == SocketFunction::LISTEN)
+				return &item;
+		}
+
+		return nullptr;
+	}
+
+	void HttpServer::ProcessEvents() {
+		while (running_) {
+			fd_set waitRecv;
+			FD_ZERO(&waitRecv);
+			for (int i = 0; i < max_sockets(); i++) {
+				if ((sockets()[i].recv == LISTEN) || (sockets()[i].recv == RECEIVE))
+					FD_SET(sockets()[i].id, &waitRecv);
+			}
+
+			fd_set waitSend;
+			FD_ZERO(&waitSend);
+
+			for (int i = 0; i < max_sockets(); i++) {
+				if (sockets()[i].send == SEND)
+					FD_SET(sockets()[i].id, &waitSend);
+			}
+
+			int nfd;
+			nfd = select(0, &waitRecv, &waitSend, NULL, NULL);
+			if (nfd == SOCKET_ERROR) {
+				std::cout << "Server: Error at select(): " << WSAGetLastError() << std::endl;
+				WSACleanup();
+				return;
+			}
+
+			for (int i = 0; i < max_sockets() && nfd > 0; i++) {
+				if (FD_ISSET(sockets()[i].id, &waitRecv)) {
+					nfd--;
+					switch (sockets()[i].recv) {
+					case LISTEN:
+						socket_service_.acceptConnection(i);
+						break;
+
+					case RECEIVE:
+						socket_service_.receiveMessage(i);
+						break;
+					}
+				}
+			}
+
+			for (int i = 0; i < max_sockets() && nfd > 0; i++) {
+				if (FD_ISSET(sockets()[i].id, &waitSend)) {
+					nfd--;
+					switch (sockets()[i].send) {
+					case SEND:
+						socket_service_.sendMessage(i);
+						break;
+					}
+				}
+			}
+		}
+
+		closesocket(socket_service_.findListeningSocket()->id);
+		WSACleanup();
 	}
 }
--- http-server/HttpServer.h
+++ http-server/HttpServer.h
@@ -44,8 +44,9 @@ namespace mta_http_server {
         sockaddr_in server_service_;
         std::vector<SOCKET_STATE> sockets_;
         int sockets_amount_;
+        int max_sockets_;
     public:
-        SocketService() : server_service_(), sockets_(std::vector<SOCKET_STATE>()), sockets_amount_(0) {}
+        SocketService() : server_service_(), sockets_(std::vector<SOCKET_STATE>()), sockets_amount_(0), max_sockets_(Settings::MAX_SOCKETS) {}
         ~SocketService() = default;
         SocketService(SocketService&&) = default;
         SOCKET_STATE* findListeningSocket();
@@ -54,6 +55,18 @@ namespace mta_http_server {
         void acceptConnection(int index);
         void receiveMessage(int index);
         void sendMessage(int index);
+
+        void SetSocketsAmount(int value) { sockets_amount_ = value; }
+        void SetServerService(ADDRESS_FAMILY sin_family, ULONG addr, uint16_t port) {
+            server_service_.sin_family = sin_family;
+            server_service_.sin_addr.s_addr = addr;
+            server_service_.sin_port = htons(port);
+        }
+
+        const std::vector<SOCKET_STATE>& sockets() const { return sockets_;  }
+        const int max_sockets() const { return max_sockets_;  }
+        const int sockets_amount() const { return sockets_amount_; }
+        const sockaddr_in& server_service() const { return server_service_; }
     };
 
 	class HttpServer {
@@ -67,8 +80,7 @@ namespace mta_http_server {
     public:
         HttpServer() = default;
         explicit HttpServer(const std::string& host, std::uint16_t port) : 
-            host_(host), port_(port), running_(false),
-            request_handlers_(), socket_service_(SocketService()) { }
+            host_(host), port_(port), running_(false), request_handlers_(), socket_service_(SocketService()) { }
         HttpServer& operator=(HttpServer&&) = default;
         ~HttpServer() = default;
 
@@ -90,9 +102,11 @@ namespace mta_http_server {
 
         HttpResponse HandleHttpRequest(const HttpRequest& request);
 
-        std::string host() const { return host_; }
-        std::uint16_t port() const { return port_; }
-        bool running() const { return running_; }
+        const std::vector<SOCKET_STATE>& sockets() const { return socket_service_.sockets(); }
+        const int max_sockets() const { return socket_service_.max_sockets(); }
+        const std::string host() const { return host_; }
+        const std::uint16_t port() const { return port_; }
+        const bool running() const { return running_; }
     };
 }
 #endif //HTTP_SERVER_H
