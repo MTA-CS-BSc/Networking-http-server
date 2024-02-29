015e621bf1cecfbc250d52ddef702cf227e42675
Maya Raskin <mayaraskin14@gmail.com>
2024-03-01 00:03:44 +0200

    Removed redundantparams from Uri

--- http-server/Uri.h
+++ http-server/Uri.h
@@ -5,15 +5,10 @@
 #include <algorithm>
 
 namespace mta_http_server {
-    // A Uri object will contain a valid scheme (for example: HTTP), host,
-    // port, and the actual URI path
+    // A Uri object will contain the actual URI path
     class Uri {
     private:
-        // Only the path is supported for now
         std::string path_;
-        std::string scheme_;
-        std::string host_;
-        unsigned int port_;
 
         void SetPathToLowercase() {
             std::transform(path_.begin(), path_.end(), path_.begin(),
@@ -34,9 +29,6 @@ namespace mta_http_server {
             SetPathToLowercase();
         }
 
-        std::string scheme() const { return scheme_; }
-        std::string host() const { return host_; }
-        unsigned int port() const { return port_; }
         std::string path() const { return path_; }
     };
 
