fb7f5d6905eba1297a1f302c0aa38e9cb58132d6
Maya Raskin <mayaraskin14@gmail.com>
2024-03-01 13:19:55 +0200

    Removed redundant constexpressions

--- http-server/Settings.h
+++ http-server/Settings.h
@@ -6,10 +6,7 @@ namespace mta_http_server {
 	public:
 		static constexpr size_t MAX_BUFFER_SIZE = 4096;
 		static constexpr unsigned short PORT = 8080;
-		static constexpr int BACK_LOG_SIZE = 1000;
 		static constexpr char HOST[] = "0.0.0.0";
-		static constexpr int K_MAX_CONNECTIONS = 10000;
-		static constexpr int K_THREAD_POOL_SIZE = 5;
 		static constexpr int MAX_SOCKETS = 100;
 	};
 }
